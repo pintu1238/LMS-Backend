637628f40c571ed80d7aab5e6933ccb86718f3d1
Nitin Kumar <112533120+nitinkumar9877@users.noreply.github.com>
2025-09-04 13:13:07 +0530

    setup

---
+++ backendProject/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import styles from './page.module.css';
+import HomePage from './homepage/page';
+
+export default function Home() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
